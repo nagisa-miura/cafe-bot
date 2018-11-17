7409bb66a4abe6457712b28f81b2e6b49038eadc
EC2 Default User <ec2-user@ip-172-31-8-201.us-east-2.compute.internal>
2018-11-17 07:26:48 +0000

    add

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -3,8 +3,8 @@ require 'open-uri'
 require 'rexml/document'
 
 class LinebotController < ApplicationController
-  protect_from_forgery :except => [:callback]
-
+  protect_from_forgery with: :null_session
+   
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
@@ -14,52 +14,50 @@ class LinebotController < ApplicationController
 
   def callback
     body = request.body.read
-
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
+    unless @client.validate_signature(body, request.env['HTTP_X_LINE_SIGNATURE'])
       error 400 do 'Bad Request' end
     end
-
-    events = client.parse_events_from(body)
-
-    events.each { |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
-        end
-        #位置情報送信時
-        when Line::Bot::Event::MessageType::Location
-          message = {
-            type: 'text',
-            text: return_location_height(event.message)
-            }
-        else
-            message = {
-            type: 'text',
-            text: "メッセージか位置情報を送ってね"
-            }
-        @client.reply_message(event['replyToken'], message)
-        render :nothing => true, status: :ok
-      end
-    }
-
-    private
-    
-    def return_location_height(message)
-        ret_msg = message['address'] + "の標高は"
-        lat = message['latitude']
-        lon = message['longitude']
-        body = open("http://lab.uribou.net/ll2h/?ll=#{lat},#{lon}", &:read)
-        doc = REXML::Document.new(body)
-        ret_msg += doc.elements['result/height'].text + "mです"
-        ret_msg
+    event = @client.parse_events_from(body)[0]
+    case event.type
+    when "text"
+      message = {
+        type: 'text',
+        text: return_message
+      }
+    when "location"
+      message = {
+        type: 'text',
+        text: return_location_height(event.message)
+      }
+    else
+      message = {
+        type: 'text',
+        text: "メッセージか位置情報を送ってね"
+      }
     end
-
+    @client.reply_message(event['replyToken'], message)
+    render :nothing => true, status: :ok
   end
+
+  private
+  
+  def return_message
+    open("http://www.meigensyu.com/quotations/view/random") do |file|
+      page = file.read
+      page.scan(/<div class=\"text\">(.*?)<\/div>/).each do |meigen|
+        return meigen[0].encode("sjis")
+      end
+    end
+  end
+  
+  def return_location_height(message)
+    ret_msg = message['address'] + "の標高は"
+    lat = message['latitude']
+    lon = message['longitude']
+    body = open("http://lab.uribou.net/ll2h/?ll=#{lat},#{lon}", &:read)
+    doc = REXML::Document.new(body)
+    ret_msg += doc.elements['result/height'].text + "mです"
+    ret_msg
+  end
+
 end
