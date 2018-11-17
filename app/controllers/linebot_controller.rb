6ae17d6d8ba9074aadba5f2a0ebdcf415c0f3029
EC2 Default User <ec2-user@ip-172-31-8-201.us-east-2.compute.internal>
2018-11-17 07:50:03 +0000

    restart

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,67 +1,65 @@
-require 'line/bot'
-require 'open-uri'
-require 'rexml/document'
+class LinebotController < ApplicationController
+  require 'line/bot' 
 
-class WebhookController < ApplicationController
-  protect_from_forgery with: :null_session
-  before_action :set_line_client
+  protect_from_forgery :except => [:callback]
 
-  CHANNEL_SECRET = '3dc7c9a5de885798c35da460eaf7992a'
-  CHANNEL_ACCESS_TOKEN = '5aZV96SXXI2N9JfIgkOE6/rG7cqVMedRaYs0/5039DSSTWy1qHYVHL6WMCfRUtiKZyj4XFqe/mGttPeYnYA2L3P92FId5LOkSxOiVYQLuaUZ//e5PXgPUkcunzvLR8jRQBCFVdqU6kdvbCCqwp0iBwdB04t89/1O/w1cDnyilFU='
-
-  def callback
-    body = request.body.read
-    unless @client.validate_signature(body, request.env['HTTP_X_LINE_SIGNATURE'])
-      error 400 do 'Bad Request' end
-    end
-    event = @client.parse_events_from(body)[0]
-    case event.type
-    when "text"
-      message = {
-        type: 'text',
-        text: return_message
-      }
-    when "location"
-      message = {
-        type: 'text',
-        text: return_location_height(event.message)
-      }
-    else
-      message = {
-        type: 'text',
-        text: "メッセージか位置情報を送ってね"
-      }
-    end
-    @client.reply_message(event['replyToken'], message)
-    render :nothing => true, status: :ok
-  end
-
-  private
-  
-  def return_message
-    open("http://www.meigensyu.com/quotations/view/random") do |file|
-      page = file.read
-      page.scan(/<div class=\"text\">(.*?)<\/div>/).each do |meigen|
-        return meigen[0].encode("sjis")
-      end
-    end
-  end
-  
-  def return_location_height(message)
-    ret_msg = message['address'] + "の標高は"
-    lat = message['latitude']
-    lon = message['longitude']
-    body = open("http://lab.uribou.net/ll2h/?ll=#{lat},#{lon}", &:read)
-    doc = REXML::Document.new(body)
-    ret_msg += doc.elements['result/height'].text + "mです"
-    ret_msg
-  end
-
-  def set_line_client
+  def client
     @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = CHANNEL_SECRET
-      config.channel_token = CHANNEL_ACCESS_TOKEN
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
 
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+
+    events = client.parse_events_from(body)
+
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        
+        #位置情報送信時
+        when Line::Bot::Event::MessageType::Location
+           message = {
+            type: 'text',
+            text: return_location_height(event.message)
+            }
+        else
+            message = {
+            type: 'text',
+            text: "メッセージか位置情報を送ってね"
+        }
+        end
+        @client.reply_message(event['replyToken'], message)
+        render :nothing => true, status: :ok
+      end
+    }
+
+    private
+    
+    def return_location_height(message)
+        ret_msg = message['address'] + "の標高は"
+        lat = message['latitude']
+        lon = message['longitude']
+        body = open("http://lab.uribou.net/ll2h/?ll=#{lat},#{lon}", &:read)
+        doc = REXML::Document.new(body)
+        ret_msg += doc.elements['result/height'].text + "mです"
+        ret_msg
+    end
+
+    head :ok
+  end
 end
